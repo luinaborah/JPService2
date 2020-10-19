6c7c65ec63a53286c7048da19814a48bf175ea36
Luina Borah <luina.borah@publicissapient.com>
2020-10-19 20:15:57 +0530

    Demo JPService2 to get its health

---
+++ src/main/java/com/jpservice2/JpService2Application.java
@@ -0,0 +1,14 @@
+package com.jpservice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@EnableDiscoveryClient
+@SpringBootApplication
+public class JpService2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpService2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpservice1/jpservice1/JpService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpservice1.jpservice1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpService1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
